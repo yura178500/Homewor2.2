f483becaf38b0011f3e4af511fa1b957f25e88f0
yura178500 <yura178500@yandex.ru>
2022-10-04 16:53:32 +0300

    Задача 6-7

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+    public static void main(String[] args) {
+        //Задача 6
+        var boxer1 = 78.2;
+        System.out.println(boxer1);
+        var boxer2 = 82.7;
+        System.out.println(boxer2);
+        var boxer = boxer1 + boxer2;
+        System.out.println(boxer);
+
+        //Задача 7.1
+        var boxer1 = 78.2;
+        System.out.println(boxer1);
+        var boxer2 = 82.7;
+        System.out.println(boxer2);
+        var weight = boxer2 -boxer1;
+        System.out.println(weight);
+
+        //Задача 7.2
+        var boxer1 = 78.2;
+        System.out.println(boxer1);
+        var boxer2 = 82.7;
+        System.out.println(boxer2);
+        var weight = boxer2 % boxer1;
+        System.out.println(weight);
+
+
+}
